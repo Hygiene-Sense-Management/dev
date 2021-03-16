0a9959783e31333ee495b1b76e622c2b2f5e4048
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-03-16 16:19:27 +0100

    updated documentation

---
+++ jstopics/device-type-groups.js
@@ -0,0 +1,12 @@
+hmLoadTopic({
+hmKeywords:"",
+hmTitle:"Device type groups",
+hmDescription:"Device type groups are global catalog elements that are used to group Device types by specific characteristics. This allows clients of HsM to handle groups of devices in a...",
+hmPrevLink:"visibility.html",
+hmNextLink:"dtws.html",
+hmParentLink:"cats.html",
+hmBreadCrumbs:"<a href=\"neutral-backend-infrastructure.html\">Neutral backend infrastructure<\/a> &gt; <a href=\"cats.html\">Catalog service<\/a>",
+hmTitlePath:"Neutral backend infrastructure > Catalog service > Device type groups",
+hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Device type groups<\/span><\/h1>\n\r",
+hmBody:"<p class=\"p_Normal\"><span class=\"f_Identifier\">Device type groups<\/span> are global catalog elements that are used to group <a href=\"cats-device-type.html\" class=\"topiclink\">Device types<\/a> by specific characteristics. This allows clients of HsM to handle groups of devices in a tailored way as well as additional <a href=\"clse-devicefilter.html\" class=\"topiclink\">Device filter<\/a> options.<\/p>\n\r"
+})
