23391e8ced2e20ea9455ef370761c23b94bf889f
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-03-17 08:03:55 +0100

    updated documentation

--- jstopics/roles.js
+++ jstopics/roles.js
@@ -1,12 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
 hmTitle:"Roles",
-hmDescription:"Enter topic text here.",
+hmDescription:"A role is a collection of permissions on a specific object (customer, organization)",
 hmPrevLink:"permissions.html",
 hmNextLink:"customer-roles.html",
 hmParentLink:"authorization2.html",
 hmBreadCrumbs:"<a href=\"backend-infrastructure.html\">Backend infrastructure<\/a> &gt; <a href=\"clse.html\">Client Service<\/a> &gt; <a href=\"authorization2.html\">Authorization<\/a>",
 hmTitlePath:"Backend infrastructure > Client Service > Authorization > Roles",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Roles<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">Enter topic text here.<\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">A role is a collection of permissions on a specific object (customer, organization)<\/p>\n\r<p class=\"p_Normal\">A user is granted a role on a specific object (global, customer, organization)<\/p>\n\r"
 })
