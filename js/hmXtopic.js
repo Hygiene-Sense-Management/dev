1744f963024a6938967d9442a4ad0ede9d4405b8
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-03-19 13:19:04 +0100

    updated documentation

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2021 Hagleitner",
 	summary: "&nbsp;",
 	version: "1.0.0",
-	date: "Wednesday, March 17, 2021",
+	date: "Friday, March 19, 2021",
 	language: "en-us",
 	mainfile: "index.html"
 	}
--- jstopics/dlms-data-link-monitoring-gateway-backend.js
+++ jstopics/dlms-data-link-monitoring-gateway-backend.js
@@ -1,12 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
-hmTitle:"Monitoring data link between stationary gateways and the HsM backend",
-hmDescription:"The Data Link Monitoring Service monitors the data link between stationary gateways and the HsM Backend. The technical environment allows to immediately recognize when a...",
+hmTitle:"Data link between stationary gateways and the HsM backend",
+hmDescription:"The Data Link Monitoring Service monitors the data link between stationary gateways and the HsM Backend. ",
 hmPrevLink:"dlms-data-link-monitoring-nongateway-gateway.html",
 hmNextLink:"dlms-data-link-monitoring-logic.html",
 hmParentLink:"dlms.html",
 hmBreadCrumbs:"<a href=\"backend-infrastructure.html\">Backend infrastructure<\/a> &gt; <a href=\"dlms.html\">Data Link Monitoring Service<\/a>",
-hmTitlePath:"Backend infrastructure > Data Link Monitoring Service > Monitoring data link between stationary gateways and the HsM backend",
-hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Monitoring data link between stationary gateways and the HsM backend<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">The Data Link Monitoring Service monitors the data link between stationary gateways and the HsM Backend. The technical environment allows to immediately recognize when a stationary-gateway connects to the HsM Backend. Disconnection to the HsM Backend will be recognized with a delay of maximum xx minutes.<\/p>\n\r"
+hmTitlePath:"Backend infrastructure > Data Link Monitoring Service > Data link between stationary gateways and the HsM backend",
+hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Data link between stationary gateways and the HsM backend<\/span><\/h1>\n\r",
+hmBody:"<p class=\"p_Normal\">The Data Link Monitoring Service monitors the data link between stationary gateways and the HsM Backend. <\/p>\n\r<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Hearbeatet<\/span><\/h1>\n\r<p class=\"p_Normal\">HsM stationary gateways send hearbeat messages that indicate to the backen that the device is alive. The hearbeat message also includes the seconds at which the device plans to send the next heartbeat<\/p>\n\r<p class=\"p_Normal\">Next hearbeatet<\/p>\n\r<p class=\"p_Normal\">The technical environment allows to immediately recognize when a stationary-gateway connects to the HsM Backend. Disconnection to the HsM Backend will be recognized with a delay of maximum xx minutes.<\/p>\n\r"
 })
