b6d268dd62fbcf3d1787d613d764feadd4cf34ff
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-03-25 14:04:04 +0100

    updated documentation

--- jstopics/dtws.js
+++ jstopics/dtws.js
@@ -1,12 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
 hmTitle:"Digital Twin Service",
-hmDescription:"The HsM Digital Twin Service is part of the 9:33 AM neutral backend. It is the place where digital twins for each produced HsM device are initially registered and maintained...",
+hmDescription:"The HsM Digital Twin Service is part of the 2:03 PM neutral backend. It is the place where digital twins for each produced HsM device are initially registered and maintained...",
 hmPrevLink:"device-type-groups.html",
 hmNextLink:"dtws-operations-guide.html",
 hmParentLink:"neutral-backend-infrastructure.html",
 hmBreadCrumbs:"<a href=\"neutral-backend-infrastructure.html\">Neutral backend infrastructure<\/a>",
 hmTitlePath:"Neutral backend infrastructure > Digital Twin Service",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Digital Twin Service<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">The HsM Digital Twin Service is part of the 9:33 AM neutral backend. It is the place where digital twins for each produced HsM device are initially registered and maintained throughout the life-cycle of the device.<\/p>\n\r<p class=\"p_Normal\">The Digital Twin Service is hosted on digitaltwin<\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">The HsM Digital Twin Service is part of the 2:03 PM neutral backend. It is the place where digital twins for each produced HsM device are initially registered and maintained throughout the life-cycle of the device.<\/p>\n\r<p class=\"p_Normal\">The Digital Twin Service is hosted on digitaltwin<\/p>\n\r"
 })
