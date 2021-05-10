1e07d467cd96cd7c9412febb2a7e0fdd7866d01f
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-05-10 06:34:13 +0200

    updated documentation

--- jstopics/dlms-data-link-status.js
+++ jstopics/dlms-data-link-status.js
@@ -1,12 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
 hmTitle:"Data-link status",
-hmDescription:"The data-link status describes connection of devices with the HsM Backend depending on the actual physical connection states as well as the expectation the HsM Backend has...",
+hmDescription:"The data-link status describes connection of devices with the HsM Backend based on the actual physical connection state (online, offline), the expectation the HsM Backend has...",
 hmPrevLink:"dlms-connection-metrics.html",
 hmNextLink:"dlms-data-link-status-ble-networked.html",
 hmParentLink:"dlms-connection-metrics.html",
 hmBreadCrumbs:"<a href=\"backend-infrastructure.html\">Backend infrastructure<\/a> &gt; <a href=\"dlms.html\">Data Link Monitoring Service<\/a> &gt; <a href=\"dlms-connection-metrics.html\">Connection metrics<\/a>",
 hmTitlePath:"Backend infrastructure > Data Link Monitoring Service > Connection metrics > Data-link status",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Data-link status<\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">The data-link status describes connection of devices with the HsM Backend depending on the actual physical connection states as well as the expectation the HsM Backend has towards this connection. <\/p>\n\r<p class=\"p_Normal\">&nbsp;<\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">The <span style=\"font-style: italic;\">data-link status<\/span> describes connection of devices with the HsM Backend based on the actual physical connection state (online, offline), the <a href=\"expectation.html\" class=\"topiclink\">expectation<\/a> the HsM Backend has towards this connection, and the time a device spent in a physical connection state. <\/p>\n\r<p class=\"p_Normal\">&nbsp;<\/p>\n\r"
 })
