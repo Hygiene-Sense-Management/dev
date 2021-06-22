3a5264c0cec2f598a15a63ac1c7e8c74ac13f74f
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-06-22 14:04:19 +0200

    updated documentation

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2021 Hagleitner",
 	summary: "&nbsp;",
 	version: "1.0.0",
-	date: "Sunday, June 20, 2021",
+	date: "Tuesday, June 22, 2021",
 	language: "en-us",
 	mainfile: "index.html"
 	}
