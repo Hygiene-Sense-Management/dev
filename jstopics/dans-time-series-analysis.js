9b5b66810e8ae05ab7c9b2dcd91d76583713b5d6
hagleitner-vizi-attila <attila.vizi@hagleitner.at>
2021-06-30 09:18:56 +0200

    updated documentation

--- jstopics/dans-time-series-analysis.js
+++ jstopics/dans-time-series-analysis.js
@@ -1,12 +1,12 @@
 hmLoadTopic({
 hmKeywords:"",
 hmTitle:"Time Series analysis",
-hmDescription:"Time series analyses provide a generic way to analyze resource instance data recorded by HsM. The time series analysis aggregates time series of resource instances over...",
+hmDescription:"Time series analyses provide a generic way to analyze numeric resource instance data recorded by HsM. The time series analysis aggregates time series of resource instances over...",
 hmPrevLink:"dans-internationalization.html",
 hmNextLink:"time-series-analysis-request.html",
 hmParentLink:"daan.html",
 hmBreadCrumbs:"<a href=\"backend-infrastructure.html\">Backend infrastructure<\/a> &gt; <a href=\"daan.html\">Data Analysis Service<\/a>",
 hmTitlePath:"Backend infrastructure > Data Analysis Service > Time Series analysis ",
 hmHeader:"<h1 class=\"p_Heading1\"><span class=\"f_Heading1\">Time Series analysis <\/span><\/h1>\n\r",
-hmBody:"<p class=\"p_Normal\">Time series analyses provide a generic way to analyze resource instance data recorded by HsM. The time series analysis aggregates time series of resource instances over time-partitions and over resource instances. The result of a time-series analysis is a set of time-series or a single time-series.<\/p>\n\r"
+hmBody:"<p class=\"p_Normal\">Time series analyses provide a generic way to analyze numeric resource instance data recorded by HsM. The time series analysis aggregates time series of resource instances over time-partitions and over resource instances. The result of a time-series analysis is a set of time-series or a single time-series.<\/p>\n\r"
 })
